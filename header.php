471ad066cc4b17a1ce41754c3878034969f43d0c
trunghien123 <44777619+trunghien123@users.noreply.github.com>
2019-03-21 21:00:07 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-inverse">
+      <div class="container-fluid" >
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>                        
+            </button>
+            <a class="navbar-brand" href="#">Logo</a>
+          </div>
+          <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+              <li><a href="index.php">Trang chủ</a></li>
+              <li><a href="gioithieu.php">Giới thiệu</a></li>
+              <li><a href="#">Projects</a></li>
+              <li><a href="require('header.php');">Liên hệ</a></li>
+            </ul>  
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#" style="padding-left: 0px">Đăng kí</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#"><span class="glyphicon glyphicon-user"></span> Đăng nhập</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Giỏ hàng</a></li>
+            </ul>
+          </div>
+      </div>
+</nav>
--- index.php
+++ index.php
@@ -9,10 +9,11 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <script src="js/dungchung.js"></script>
   <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/taikhoan.css">
 </head>
 <body>
   <!-- header-->
-    <script>header();</script>
+    <?php require("header.php");?>
   <!--slide show-->
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
         <!-- Indicators -->
@@ -31,18 +32,12 @@
 
           <div class="item">
             <img src="https://shopdongho.com/wp-content/uploads/2018/09/banner-shop-dong-ho-3.jpg" alt="Image">
-            <div class="carousel-caption">
-              <!-- <h3>Sell $</h3>
-              <p>Money Money.</p> -->
-            </div>      
+            <div class="carousel-caption"></div>      
           </div>
 
           <div class="item">
-            <img src="https://www.pnj.com.vn/images/promo/49/Banner-dong-ho-tra-gop1200x450.png" alt="Image">
-            <div class="carousel-caption">
-              <!-- <h3>Sell $</h3>
-              <p>Money Money.</p> -->
-            </div>      
+            <img src="https://www.pnj.com.vn/images/promo/49/Banner-dong-ho-tra-gop1200x450.png" alt="Image">      
+            <div class="carousel-caption"></div>
           </div>
         </div>
 
@@ -56,43 +51,99 @@
           <span class="sr-only">Next</span>
         </a>
     </div>
-  
-<div class="container text-center">    
+  <!--body-->
+<div class="row">    
  <br/>
-  <div class="  container-float-left" style="width:70%">
-    <script type="text/javascript">
-      for(var i=1;i<26;i++)
-      {
-        document.write(`<img src='https://placehold.it/150x80?text=IMAGE'  alt="Image" > &nbsp`)
-
-      }
-    </script>
+  <div class="col-md-4">
+    <div>
+      <form class="input-search" method="get" action="index.html">
+                    <div class="autocomplete">
+                        <input id="search-box" name="search" autocomplete="off" type="text" placeholder="Nhập từ khóa tìm kiếm...">
+                        <button type="submit">
+                            <i class="glyphicon glyphicon-search"></i>
+                        </button>
+                    </div>
+                </form>
+    </div><br/>
+    <table style="margin-left: 20px">
+        <tr> 
+          <td> Phân loại : </td>
+          <td>  <select style="margin-left: 10px">
+                  <option>Casio</option>
+                </select>
+          </td>
+        </tr>
+    </table><br/>
+    <table style="margin-left: 20px">
+        <tr>
+          <form>
+            <td> Khoảng giá: </td>
+            <td> <input type="text" name="giatu" size="5">~<input type="text" name="giaden" size="5"> </td>
+          </form>
+        </tr>
+    </table><br/>
+    <table style="margin-left: 90px">
+        <tr><td align="text-center" colspan="2"><input type="submit" name="timkiem" value="Tìm kiếm"></td></tr>
+    </table>
+    <br/>
   </div>
-  <div style="float: right;width: 22%">
-    <div>
-      <form class="input-search" method="get" action="index.html">
-                    <div class="autocomplete">
-                        <input id="search-box" name="search" autocomplete="off" type="text" placeholder="Nhập từ khóa tìm kiếm...">
-                        <button type="submit">
-                            <i class="glyphicon glyphicon-search"></i>
-                        </button>
-                    </div>
-                </form>
-    </div>
-    Tìm kiếm nâng cao: <br/>
-    <div>
-      <form class="input-search" method="get" action="index.html">
-                    <div class="autocomplete">
-                        <input id="search-box" name="search" autocomplete="off" type="text" placeholder="Nhập từ khóa tìm kiếm...">
-                        <button type="submit">
-                            <i class="glyphicon glyphicon-search"></i>
-                        </button>
-                    </div>
-                </form>
-    </div>
-      <!--<div class="well">
-       <p>Some text..</p>
-      </div>-->
+  <div class="col-md-8">
+    
+      <div class="col-sm-6 khungsp">
+          <img src="images/casio1.png" width="180px">
+          <button class="nutgiohang">
+              <i class="glyphicon glyphicon-shopping-cart"></i> Thêm vào giỏ hàng
+          </button>
+      </div>
+      <div class="col-sm-6 khungsp ">
+          <img src="images/casio1.png" width="180px">
+          <button class="nutgiohang">
+              <i class="glyphicon glyphicon-shopping-cart"></i> Thêm vào giỏ hàng
+          </button>
+      </div>
+      <div class="col-sm-6 khungsp">
+          <img src="images/casio1.png" width="180px">
+          <button class="nutgiohang">
+              <i class="glyphicon glyphicon-shopping-cart"></i> Thêm vào giỏ hàng
+          </button>
+      </div>
+      <div class="col-sm-6 khungsp">
+          <img src="images/casio1.png" width="180px">
+          <button class="nutgiohang">
+              <i class="glyphicon glyphicon-shopping-cart"></i> Thêm vào giỏ hàng
+          </button>
+      </div>
+      <div class="col-sm-6 khungsp">
+          <img src="images/casio1.png"width="180px">
+          <button class="nutgiohang">
+              <i class="glyphicon glyphicon-shopping-cart"></i> Thêm vào giỏ hàng
+          </button>
+      </div>
+      <div class="col-sm-6 khungsp">
+          <img src="images/casio1.png"width="180px">
+          <button class="nutgiohang">
+              <i class="glyphicon glyphicon-shopping-cart"></i> Thêm vào giỏ hàng
+          </button>
+      </div>
+      <div class="col-sm-6 khungsp">
+          <img src="images/casio1.png"width="180px">
+          <button class="nutgiohang">
+              <i class="glyphicon glyphicon-shopping-cart"></i> Thêm vào giỏ hàng
+          </button>
+      </div>
+      <div class="col-sm-6 khungsp">
+          <img src="images/casio1.png"width="180px">
+          <button class="nutgiohang">
+              <i class="glyphicon glyphicon-shopping-cart"></i> Thêm vào giỏ hàng
+          </button>
+      </div>
+      <div class="col-sm-6 khungsp">
+          <img src="images/casio1.png"width="180px">
+          <button class="nutgiohang">
+              <i class="glyphicon glyphicon-shopping-cart"></i> Thêm vào giỏ hàng
+          </button>
+      </div>
+      
   </div>
 </div><br>
 
